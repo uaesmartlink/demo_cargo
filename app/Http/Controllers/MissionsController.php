ac109ac12f3ffcca630fb874bd67c8975691cfd5
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-29 13:09:05 +0400

    report pagpaginate remove

--- app/Http/Controllers/MissionsController.php
+++ app/Http/Controllers/MissionsController.php
@@ -330,8 +330,7 @@ class MissionsController extends Controller
             }
 
         }
-        $missions = $missions->orderBy('id','DESC');
-        dd($mission);
+        $missions = $missions->orderBy('id','DESC')->paginate(20);
         $page_name = translate('Missions Report');
         $status = 'all';
 

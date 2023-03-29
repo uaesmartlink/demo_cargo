dc1f71853a75378a35ee27e9c591d960962252c3
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-29 14:04:15 +0400

    amount

--- resources/views/backend/missions/missions-report-pdf.blade.php
+++ resources/views/backend/missions/missions-report-pdf.blade.php
@@ -172,13 +172,12 @@
                                             <td>{{translate('No Driver')}}</td>
                                         @endif
                                         @php
-                                            $helper = new \App\Http\Helpers\TransactionHelper();
-                                            $amount = $mission->shipment_mission[0]->shipment->amount_to_be_collected + $mission->shipment_mission[0]->shipment->shipping_cost;
-                                            // $shipment_cost = $helper->calcMissionShipmentsAmount($mission->getOriginal('type'),$mission->id);
-                                            $total_amount += $amount;
-                                        @endphp
-
-                                        <td>{{format_price($shipment_cost)}}</td>
+                                        // $helper = new \App\Http\Helpers\TransactionHelper();
+                                        $amount = $mission->shipment_mission[0]->shipment->amount_to_be_collected + $mission->shipment_mission[0]->shipment->shipping_cost;
+                                        // $shipment_cost = $helper->calcMissionShipmentsAmount($mission->getOriginal('type'),$mission->id);
+                                        $total_amount += $amount;
+                                    @endphp
+                                    <td>{{format_price($amount)}}</td>
                                         {{-- @if(isset($show_due_date)) <td>{{$mission->due_date ?? "-"}}</td> @endif --}}
                                         <td><span class="btn btn-sm btn-{{\App\Mission::getStatusColor($mission->status_id)}}">{{$mission->getStatus()}}</span></td>
                                     </tr>
